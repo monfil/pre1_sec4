dcd4bbf82382d53458f986d87397da61e9a0b0b2
monfil <fabiolamonfil@gmail.com>
2017-01-16 13:31:46 -0600

    Sección 4 terminada

--- shortest.rb
+++ shortest.rb
@@ -1,17 +1,15 @@
-def shortest(*arr)
-	resa =[]
-	begin 
-		res = arr.min {|a,b| a.length <=> b.length}
-		resa  << res
-		if resa[0].length != resa.last.length
-			resa.delete (res)
-			break
-		end
-		arr.delete(res)
-	end while resa[0].length == res.length
-	p resa
-end
+def shortest (arr)
+  arr2 = arr[1]
+  short = []
+  arr.each do |palabra|
+    if arr2.length >= palabra.length
+      arr2 = palabra
+      short << palabra
+    end
+  end
+  short
+end  
 
-p shortest("uno", "dos", "tres", "cuatro", "cinco") == ["uno", "dos"]
-p shortest("gato", "perro", "elefante", "jirafa") == ["gato"]
-p shortest("verde", "rojo", "negro", "morado") == ["rojo"]
+p shortest(['uno', 'dos', 'tres', 'cuatro', 'cinco']) == ["uno", "dos"]
+p shortest(['gato', 'perro', 'elefante', 'jirafa']) == ["gato"]
+p shortest(['verde', 'rojo', 'negro', 'morado']) == ["rojo"] 
